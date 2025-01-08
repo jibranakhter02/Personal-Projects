5be14454ff47c9bd39669d5e4e8e26775ee63b72
Mohammed Jibran Akhter <jibranakhtar6@gmail.com>
2025-01-08 19:43:09 +0400

    GUI Initial Setup

---
+++ Expense_Statement_Generator.py
@@ -0,0 +1,10 @@
+import customtkinter as ctk
+import tkinter.messagebox as tkmb
+
+# GUI setup
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
+
+app = ctk.cTk()
+app.geometry("500x500")
+app.title("Expense Statement Generator")
